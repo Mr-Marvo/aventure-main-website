f5dc5b325aa25aa63b64bc7435af7929ef3a7941
Kasunisarangi <kasuni.aventure@gmail.com>
2025-03-19 13:29:49 +0530

    add video.mp4

--- src/app/sections/VideoSection.tsx
+++ src/app/sections/VideoSection.tsx
@@ -1,19 +1,18 @@
-import Image from "next/image";
-
 const VideoSection = () => {
   return (
     <section className="w-full h-screen flex bg-cover overflow-hidden relative">
       {/* Background Blur Effect */}
-      {/* <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-gray-900/40 to-black/80 backdrop-blur-2xl"></div> */}
+      <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-gray-900/40 to-black/80 backdrop-blur-2xl"></div>
 
       {/* Video Section */}
-      <div className="relative z-10">
-        <Image
-          src="/video-section.png"
-          width={1640}
-          height={500}
-          alt="Picture of the author"
-          className="object-cover"
+      <div className="relative z-10 w-full h-full">
+        <video
+          src="/video.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover"
         />
       </div>
     </section>
