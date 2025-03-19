5e2c4b88c0c92675360cb97500938ff6d193ac51
Kasunisarangi <kasuni.aventure@gmail.com>
2025-03-19 13:02:08 +0530

    fix new issue

--- src/app/sections/Vision.tsx
+++ src/app/sections/Vision.tsx
@@ -7,7 +7,7 @@ const Vision = () => {
       <div className="flex text-lg sm:text-2xl md:text-4xl lg:text-4xl xl:text-5xl font-normal pb-6 sm:pb-9 text-center sm:text-right">
         <h1>
           Deliver innovative technology solutions for businesses across various
-          industries. Since 2022, we've been helping companies in finance,
+          industries. Since 2022, we&apos;ve been helping companies in finance,
           healthcare, retail, and more with custom software designed to meet
           their specific needs.
         </h1>
@@ -19,8 +19,8 @@ const Vision = () => {
         <div className="flex w-full lg:w-1/2">
           <div className="bg-gray-100 p-6 sm:p-10 md:p-14 rounded-3xl flex flex-col pt-10 sm:pt-20 justify-center">
             <p className="text-gray-800 text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-normal">
-              Whether you're looking to revamp your digital presence or build a
-              custom solution from the ground up,{" "}
+              Whether you&apos;re looking to revamp your digital presence or
+              build a custom solution from the ground up,{" "}
               <span className="text-green-500 font-semibold">Aventure</span> is
               here to turn your vision into reality.
             </p>
