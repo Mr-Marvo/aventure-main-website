babc3b7b613efc70f391c897d927418293d45254
oshani madhushika <oshanimadhushika12@gmail.com>
2025-03-28 15:08:59 +0530

    make capability all section with hover animations

--- src/app/capabilities/CapabilityPage.tsx
+++ src/app/capabilities/CapabilityPage.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Squares from "../components/animations/Squares";
 import ArrowAngleRight from "../assets/svgs/ArrowAngleRight";
 import ArrowRight from "../assets/svgs/ArrowRight";
 import Slider from "../sections/Slider";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import PixelCard from "../components/animations/PixelCard";
 
 const CapabilityPage = () => {
   const [hoveredId, setHoveredId] = useState<string | null>(null);
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+  const [lettersRef, setlettersRef] = useArrayRef<HTMLSpanElement>();
+  const triggerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    setCursorPos({ x: e.clientX, y: e.clientY });
+  };
 
   const capabilities = [
     {
@@ -75,6 +85,42 @@ const CapabilityPage = () => {
     },
   ];
 
+  function useArrayRef<T>() {
+    const lettersRef = useRef<T[]>([]);
+    lettersRef.current = [];
+    return [
+      lettersRef,
+      (ref: T | null) => {
+        if (ref) lettersRef.current.push(ref);
+      },
+    ] as const;
+  }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const text =
+    "At Aventure, we don’t just provide IT solutions—we deliver innovation, security, and scalability tailored to your business needs. Here’s why businesses trust us,";
+
+  useEffect(() => {
+    if (!triggerRef.current) return;
+
+    const anim = gsap.to(lettersRef.current, {
+      scrollTrigger: {
+        trigger: triggerRef.current,
+        scrub: true,
+        start: "top 93%",
+        end: "bottom 85%",
+      },
+      color: "#000000",
+      duration: 1,
+      stagger: 1,
+    });
+
+    return () => {
+      anim.kill();
+    };
+  }, []);
+
   return (
     <>
       {/* 1st sec */}
@@ -86,9 +132,10 @@ const CapabilityPage = () => {
           borderColor="#F0F0F0"
           hoverFillColor="#F1F1F1"
         />
-        <div className="absolute z-10 text-black text-left p-5 md:p-10">
-          <div className="flex flex-col justify-start w-full items-start">
-            <p className="text-2xl md:text-3xl lg:text-5xl xl:text-6xl font-extralight leading-normal md:pl-40">
+
+        <div className="absolute z-10 text-black text-left p-10 w-full flex items-center justify-center">
+          <div className="max-w-6xl w-full">
+            <p className="text-2xl md:text-3xl lg:text-5xl xl:text-6xl font-extralight leading-normal text-end">
               We provide innovative, tailored IT
             </p>
             <p className="text-2xl md:text-3xl lg:text-5xl xl:text-6xl font-extralight leading-normal">
@@ -97,9 +144,9 @@ const CapabilityPage = () => {
             <p className="text-2xl md:text-3xl lg:text-5xl xl:text-6xl font-extralight leading-normal">
               growth.
             </p>
-            <div className="hidden md:block w-[70%]"></div>
           </div>
         </div>
+
         {/* Positioned RoundImage at bottom-left */}
         <div className="absolute bottom-20 md:bottom-10 right-2 lg:right-0  ">
           <button className="flex items-center gap-2 border border-gray-400 rounded-full px-3 py-1 text-sm font-medium hover:bg-gray-100 transition">
@@ -110,21 +157,28 @@ const CapabilityPage = () => {
       </section>
 
       {/* 2nd sec capability */}
-      <section className="w-full bg-white">
+      <section className="w-full bg-white relative">
         <div className="w-full mx-auto">
           {capabilities.map((capability) => (
             <div
               key={capability.id}
               className="flex flex-col lg:flex-row items-center justify-between hover:bg-black hover:text-white p-10 md:h-[411px]"
               onMouseEnter={() => setHoveredId(capability.id)}
+              onMouseMove={handleMouseMove}
               onMouseLeave={() => setHoveredId(null)}
             >
               {/* ID Section */}
               <div className="w-1/3 text-5xl relative hidden lg:block">
                 {capability.id}-
                 {hoveredId === capability.id && (
-                  <div className="absolute right-0 bottom-0 top-20">
-                    <button className="bg-[#73BA0D] text-white  px-5 py-2 rounded-full text-sm flex items-center gap-2 transition duration-300 z-10">
+                  <div
+                    className="absolute right-0 bottom-0 top-20"
+                    style={{
+                      top: cursorPos.y - 310,
+                      left: cursorPos.x - 50,
+                    }}
+                  >
+                    <button className="bg-[#73BA0D] text-white  px-5 py-2 rounded-full text-sm flex items-center gap-2 transition duration-300 z-10 whitespace-nowrap">
                       View Portfolio
                       <ArrowRight />
                     </button>
@@ -157,31 +211,47 @@ const CapabilityPage = () => {
       <section className="w-full bg-white px-4 py-5 lg:h-[591px] flex items-center justify-start">
         <div className=" items-center">
           <p className="text-gray-500 text-base">WHY WE</p>
-          <div className="w-full md:max-w-6xl">
-            <p className="text-4xl lg:text-6xl pl-4">
+          <div className="w-full md:max-w-5xl mt-2 pl-5">
+            {/* old text without animation */}
+            {/* <p className="text-4xl lg:text-6xl pl-4">
               At aventure, we don’t just provide IT <br /> solutions—we deliver
               innovation, security, <br /> and scalability tailored to your
               business <br />
               needs. Here’s why businesses trust us,
-            </p>
+            </p>  */}
+
+            <div ref={triggerRef}>
+              {text.split("").map((letter, index) => (
+                <span
+                  className="text-4xl md:text-6xl leading-12 md:leading-20  text-white drop-shadow-lg"
+                  key={index}
+                  ref={setlettersRef}
+                >
+                  {letter}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </section>
 
       {/* grid */}
       <section className="w-full">
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 p-6">
           {items.map((item, index) => (
-            <div
-              key={index}
-              className="bg-white px-6 rounded-lg shadow-md hover:bg-gray-200 transition-all duration-300 h-[500px] flex flex-col justify-center items-start"
-            >
-              <div className="text-start">
-                <div className="text-sm mb-4">{item.icon}</div>
-                <h3 className="text-4xl font-semibold mb-3">{item.title}</h3>
-                <p className="text-gray-600 text-xl">{item.description}</p>
+            <PixelCard variant="gray">
+              <div
+                key={index}
+                // className="absolute bg-white px-6 rounded-lg drop-shadow-lg transition-all duration-300 h-[500px] flex flex-col justify-center items-start"
+             className="absolute"
+             >
+                <div className="text-start px-20">
+                  <div className="text-sm mb-5">{item.icon}</div>
+                  <h3 className="text-4xl font-bold mb-5">{item.title}</h3>
+                  <p className="text-gray-600 text-xl">{item.description}</p>
+                </div>
               </div>
-            </div>
+            </PixelCard>
           ))}
         </div>
       </section>
