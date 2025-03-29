3549726ab68bf03fc279543ede4339c47ff5c31e
Kasunisarangi <kasuni.aventure@gmail.com>
2025-03-29 18:14:53 +0530

    updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "off", // Disabled unused variable checks
+      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off", // Completely disable the rule
       "react/no-unescaped-entities": "off", // Turns off the rule
       "react-hooks/exhaustive-deps": "off", // Disables the rule globally
