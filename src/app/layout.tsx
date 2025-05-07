be21b8540a0427c4e4ca54bab1e17bebf7559e60
Nishan Premakumara <aknpremakumara@gmail.com>
2025-05-07 10:21:45 +0530

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export const metadata: Metadata = {
   title: "Aventure",
   description: "We Make Concepts",
@@ -19,7 +22,11 @@ export default function RootLayout({
         <div className="absolute top-0 right-0 md:top-14 md:right-0 w-full">
           <Navbar />
         </div>
-        <main>{children}</main>
+        <main>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </main>
         <Footer />
       </body>
     </html>
